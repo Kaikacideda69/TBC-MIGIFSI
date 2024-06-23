be64b2799ed1a89211affac319f20243012df750
Kaikacideda69 <93218650+Kaikacideda69@users.noreply.github.com>
2024-06-23 17:19:19 +0400

    Add files via upload

---
+++ TBC-Day08/TBC-Day08/Program.cs
@@ -0,0 +1,53 @@
+﻿// Task 1
+
+/*using System; 
+
+namespace Start
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNum();
+        }
+        static void PrintNum()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}*/
+
+
+// Task 2
+
+/*using System;
+
+namespace Start
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PrintNum(number));
+        }
+        static int PrintNum(int num)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                sum += i; 
+            }
+
+           return sum;
+        }
+        
+    }
+}*/
